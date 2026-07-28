fa9055d3ac658f09791ad971acef1292bc4f04bb
niharikamalyala <niharikamalyala@usf.edu>
2026-07-28 16:37:35 -0400

    Add initial news ingestion module

---
+++ pyspark/ingest_news.py
@@ -0,0 +1,22 @@
+"""
+AI News Sentiment Pipeline
+
+Module:
+News Ingestion
+
+Author:
+Niharika Malyala
+
+Description:
+This module is responsible for collecting news
+articles from external APIs before they are
+processed using PySpark.
+"""
+
+
+def main():
+    print("Starting News Ingestion Pipeline...")
+
+
+if __name__ == "__main__":
+    main()
